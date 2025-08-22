02f294d756812514f2726ea98979a2a3e6e2366a
Kirill <cryt.mem@gmail.com>
2025-08-22 12:49:38 +0300

    sol

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,18 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
+        for (int i = 1; i <= cathetusLength; i++) {
 
+            for (int s = 1; s <= cathetusLength - i; s++) {
+                System.out.print(" ");
+            }
+
+            for (int j = i; j >= 1; j--) {
+                System.out.print('*');
+            }
+
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,27 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        for (int i = 1; i <= cathetusLength; i++) {
 
+            for (int s = 1; s <= cathetusLength - i; s++) {
+                System.out.print(" ");
+            }
+
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j);
+            }
+
+            for (int s = 1; s <= cathetusLength - i; s++) {
+                System.out.print(" ");
+            }
+
+
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
